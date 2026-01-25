a9944f57fee0a4b075c4c2a9d3dff3ebccc2963b
PaulFirs <makloun@hotmail.com>
2026-01-25 20:40:25 +0300

    Refactor remote debugging setup and configuration files for improved clarity and functionality

--- .vscode/remote_debug_start.sh
+++ .vscode/remote_debug_start.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
-host_name="${bm_remoteHostName:-${1}}"
-remote_host="${bm_remoteHost:-${2}}"
+host_name="${remote_debug_host_name:-${1}}"
+ip_host="${remote_debug_host:-${2}}"
+password="${remote_debug_password:-}"
 
-if [ -z "$host_name" ] || [ -z "$remote_host" ]; then
+if [ -z "$host_name" ] || [ -z "$ip_host" ]; then
     echo "Usage: $0 <host_name> <remote_host> or set bm.remoteHostName and bm.remoteHost in settings.json"
     exit 1
 fi
 
-export REMOTE_DEBUG_HOST="$host_name@$remote_host"
+export REMOTE_DEBUG_HOST="$host_name@$ip_host"
 
 
 echo "Starting remote debug"
-echo "Stopping previous JLinkGDBServer processes on remote host (if any)"
-if [ -n "$REMOTE_DEBUG_PASS" ]; then
-    SSH_CMD="sshpass -p \"$REMOTE_DEBUG_PASS\" ssh -o StrictHostKeyChecking=no"
+echo "Stopping previous JLinkRemoteServer processes on remote host (if any)"
+if [ -n "$password" ]; then
+    SSH_CMD="sshpass -p \"$password\" ssh -o StrictHostKeyChecking=no"
 else
     SSH_CMD="ssh"
 fi
 
 
-eval $SSH_CMD $REMOTE_DEBUG_HOST 'pkill -f JLinkGDBServer || true'
+KILL_RESULT=$(eval $SSH_CMD $REMOTE_DEBUG_HOST 'pkill -f JLinkRemoteServer'; echo $?)
+if [ "$KILL_RESULT" -eq 0 ]; then
+    echo "JLinkRemoteServer process was killed on remote host."
+else
+    echo "No JLinkRemoteServer process was running on remote host."
+fi
 echo "Remote host: $REMOTE_DEBUG_HOST"
 echo "Launching JLinkRemoteServer on remote host"
 JLINK_SERIAL="${bm_jlinkSerial:-772440171}"
 JLINK_DEVICE="${bm_deviceName:-STM32F765ZG}"
-eval $SSH_CMD $REMOTE_DEBUG_HOST "nohup ./JLinkRemoteServer -select usb=$JLINK_SERIAL -device $JLINK_DEVICE -endian little -speed 4000 -if swd -vd &> /tmp/remote/JLinkRemoteServer.log &"
+eval $SSH_CMD $REMOTE_DEBUG_HOST "JLinkRemoteServer -select usb=$JLINK_SERIAL -device $JLINK_DEVICE -endian little -speed 4000 -if swd -vd"
+eval $SSH_CMD $REMOTE_DEBUG_HOST "pgrep -af JLinkRemoteServer"
 echo "Remote JLinkRemoteServer started."
